db8fc717e609f482328c3e76b21fb81fd20597d5
liyabo01 <liyabo01@baidu.com>
2018-07-03 14:04:48 +0800

    add crawler

---
+++ py/crawler_news.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#_*_coding=utf-8_*_
+
+from urllib.parse import quote
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+driver = webdriver.PhantomJS()
+
+def query_baidu_news(keyword):
+    url = u'http://news.baidu.com/ns?word=' +quote(keyword) +'&tn=news&from=news&cl=2&rn=20&ct=1'
+    driver.get(url)
+    data = driver.page_source
+
+    usoup = BeautifulSoup(data, 'lxml')
+    results = usoup.find_all(class_='result')
+    for r in results:
+        a = r.find('a')
+        title = a.text.strip()
+        print('\t'.join([keyword, title]))
+
+if __name__ == '__main__':
+    query_baidu_news('人物八卦')
